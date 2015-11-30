99ed31b0e3ba09ebbec51e81843ee5135d4c7f17
Pedro Fornaza <pedro@forbit.com.br>
2015-11-30 21:33:01 -0200

    Refatorando

---
+++ data/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `postagens` (
+    `id` int auto_increment,
+    `titulo` varchar(100) not null,
+    `post` text not null,
+    primary key (id)
+);
--- index.php
+++ index.php
@@ -1,67 +1,28 @@
 <?php
 
-  include 'from/db_php/connect.php';
-  
-  // Busca via GET
-  $busca = $_GET['q'];
+$config = require 'config/db.php';
+require_once 'lib/db.php';
+require_once 'lib/busca.php';
 
-  // Resultados por página
-  $max_resultados_pagina = "10";
+$conn = connect($config['type'], $config['host'], $config['port'], $config['name'], $config['user'], $config['pass']);
 
-  // SELECT para Busca
-  $select = $connect_mysql->prepare("SELECT * FROM postagens WHERE titulo LIKE :search or post LIKE :search");
-  $select->bindParam( ":search", "%$busca%" );
-  $select->execute();
-  
+$expressao = expressaoBusca();
+$total = totalResultados($expressao, $conn);
 
-  // ...
-
-
-  // Calcula quantos resultados retornou a busca
-  $contagem_resultados = mysql_num_rows($select); 
-
-  // Adiciona o plural na palavra Resultado(s)
-  if ($contagem_resultados <= 1) { 
-    $contagem_resultados_palavra = "resultado"; 
-  } else { 
-    $contagem_resultados_palavra = "resultados"; 
-  }
-
-  // Quantidade de Resultados
-  echo "<p> <?php echo $contagem_resultados." ".$contagem_resultados_palavra;?> para sua pesquisa. </p>";
-
-
-  if ($contagem_resultados == 0) {
-   
+if ($total == 0) {
     echo "<p>Ops, nada Encontrado! Tente sua busca novamente com outras palavras!</p>";
-  
-  } else {
+    exit;
+}
 
-    while ($resultado_busca = mysql_fetch_assoc($select)) { 
+echo "<p>{$total} resultado(s) para sua pesquisa. </p>";
 
-      // Dados para exibição
-      $resultado_id = $resultado_busca['id'];
-      $resultado_titulo = $resultado_busca['titulo'];
-      $resultado_post = $resultado_busca['post'];
+$resultado = buscar($expressao, $conn);
 
-      // Se a quantidade de caracteres for maior que 42, reduza e insira '...' no final
-      if (strlen($resultado_post) >= 42) { $resultado_post = substr($resultado_post, 0, 51)."..."; }
-
-      // Exibição dos resultados da pesquisa
-      echo "<!-- RESULTADO -->
-        <div>
-          <h4>{$resultado_titulo}</h4>
-          <p>{$resultado_titulo}</p>
-          <a href='http://meublog.com.br/?post={$resultado_id}'> Link para Postagem </a>
-        </div>";
-
-    } /* Fim while */
-
-  } /* Fim if-else */
-
-
-  // Paginação
-
-  // ...
-
-?>
+$template = <<<HTML
+    <div>
+        <h4>%2\$s</h4>
+        <p>%3\$s</p>
+        <a href="http://meublog.com.br/?post=%1\$s"> Link para Postagem </a>
+    </div>
+HTML;
+exibeResultados($resultado, $template);
