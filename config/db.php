cfb9a77b11e9e71b6436dd529eea41e3ba10342b
Gabriel Masson <gabrielmasson.a3912@gmail.com>
2016-02-25 16:05:51 -0300

    up

--- config/db.php
+++ config/db.php
@@ -4,12 +4,11 @@
 $db = array(
   'type' => 'mysql',
   'host' => 'localhost:3306',
-  'port' => 3306,
   'name' => 'losuch',
   'user' => 'root',
-  'pass' => 'root',
+  'pass' => 'root'
 );
 
 # Conexão MySQL
-$connect = new PDO("{$db['type']}:host={$db['host']};port={$db['port']};dbname={$db['name']}", "{$db['user']}", "{$db['pass']}");
+$connect = new PDO("{$db['type']}:host={$db['host']};dbname={$db['name']}", "{$db['user']}", "{$db['pass']}");
 $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
